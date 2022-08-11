d328162888ec7445ace9672eab4f1dc2b2a0894b
macbookair <socheapheaktra@gmail.com>
2022-08-11 16:19:06 +0700

    Added Cloud Database

--- myDB/__init__.py
+++ myDB/__init__.py
@@ -1,10 +1,26 @@
 import mysql.connector
 
+'''
+For cloud
+
+host="b5grqlbaqnhiu3xs5zqs-mysql.services.clever-cloud.com"
+user="umirqrxz6pdfjjpo"
+passwd="4Lj01OwXEH0e5t7ZKIFU"
+port="3306"
+database="b5grqlbaqnhiu3xs5zqs"
+
+For local
+host="localhost"
+user="root"
+passwd=""
+database="bus_reservation"
+'''
+
 mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="",
-    database="bus_reservation"
+    host="b5grqlbaqnhiu3xs5zqs-mysql.services.clever-cloud.com",
+    user="umirqrxz6pdfjjpo",
+    passwd="4Lj01OwXEH0e5t7ZKIFU",
+    database="b5grqlbaqnhiu3xs5zqs"
 )
 
 mycursor = mydb.cursor()
