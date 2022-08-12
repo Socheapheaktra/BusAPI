d72ffb08d5783264de628b5b670d2549aea5bab8
macbookair <socheapheaktra@gmail.com>
2022-08-12 23:27:31 +0700

    ?

--- myDB/__init__.py
+++ myDB/__init__.py
@@ -16,11 +16,21 @@ passwd=""
 database="bus_reservation"
 '''
 
+
+"""For Using Cloud"""
 mydb = mysql.connector.connect(
     host="b5grqlbaqnhiu3xs5zqs-mysql.services.clever-cloud.com",
     user="umirqrxz6pdfjjpo",
     passwd="4Lj01OwXEH0e5t7ZKIFU",
     database="b5grqlbaqnhiu3xs5zqs"
 )
+
+"""For Using Local"""
+# mydb = mysql.connector.connect(
+#     host="localhost",
+#     user="root",
+#     passwd="",
+#     database="bus_reservation"
+# )
 
 mycursor = mydb.cursor()
